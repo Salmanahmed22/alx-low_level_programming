85798b5b9daf9d2a0a014bfaf4072523047ca27c
salman ahmed <solom.9850@gmail.com>
2023-02-16 16:56:24 -0800

    as

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it is main function
+ *
+ * Return: 0 when successfull
+ */
+
+int main (void)
+{
+	printf ("with proper grammar, but the outcome is a piece of art,, followed by a new line,\n");
+	return (0);
+}
+
