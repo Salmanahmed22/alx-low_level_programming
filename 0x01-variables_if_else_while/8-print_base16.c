c7624b4919744e609406f061eae8e32d28ac5652
salman ahmed <solom.9850@gmail.com>
2023-02-17 17:14:28 -0800

    as

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	char x;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (x = 'A' ; x <= 'F' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
