9d8292b406655e053d458c96172b541bf186e551
salman ahmed <solom.9850@gmail.com>
2023-02-22 13:18:59 -0800

    as

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -add two numbers
+ *
+ * @a: function parameter
+ * @b: function parameter
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
